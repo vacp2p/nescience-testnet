b249a50a342f3cb0fa088dcf34f34fad98cbb646
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2024-11-25 11:02:57 +0200

    fix: ci test 7

--- ci_scripts/test-ubuntu.sh
+++ ci_scripts/test-ubuntu.sh
@@ -2,7 +2,5 @@ set -e
 
 curl -L https://risczero.com/install | bash 
 /home/runner/.risc0/bin/rzup install 
-cargo install taplo-cli --locked
 
-cargo fmt -- --check
-taplo fmt --check
+cargo test --release
