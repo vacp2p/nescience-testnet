ab554877768ae0403849a15a37e6c480e46809de
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2025-06-06 11:32:18 +0300

    fix: taplo fmt template excluded

--- ci_scripts/lint-ubuntu.sh
+++ ci_scripts/lint-ubuntu.sh
@@ -6,4 +6,45 @@ source env.sh
 cargo install taplo-cli --locked
 
 cargo fmt -- --check
-taplo fmt --check
+cd accounts
+    taplo fmt --check
+cd ..
+cd common
+    taplo fmt --check
+cd ..
+cd consensus
+    taplo fmt --check
+cd ..
+cd mempool
+    taplo fmt --check
+cd ..
+cd networking
+    taplo fmt --check
+cd ..
+cd node_core
+    taplo fmt --check
+cd ..
+cd node_rpc
+    taplo fmt --check
+cd ..
+cd node_runner
+    taplo fmt --check
+cd ..
+cd sc_core
+    taplo fmt --check
+cd ..
+cd sequencer_core
+    taplo fmt --check
+cd ..
+cd storage
+    taplo fmt --check
+cd ..
+cd utxo
+    taplo fmt --check
+cd ..
+cd vm
+    taplo fmt --check
+cd ..
+cd zkvm
+    taplo fmt --check
+cd ..
